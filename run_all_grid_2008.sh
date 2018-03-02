6b3e2f5e1127b880a650f0257ed80a50c0d51fa7
Alexandre Sawczuk da Silva <sawczualex@Alexandres-MacBook-Pro.local>
2018-03-02 16:50:22 +1300

    Minor changed to grid script.

--- run_all_grid_2008.sh
+++ run_all_grid_2008.sh
@@ -2,6 +2,6 @@
 
 NUM_RUNS=50
 
-for i in {5..5}; do
+for i in {1..8}; do
   qsub -t 1-$NUM_RUNS:1 indirect_sequence.sh ~/workspace/wsc2008/Set0${i}MetaData 2008-nsga-ii-moead-ecj-hybrid${i} nsga2-indirect-sequence.params;
 done
